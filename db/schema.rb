9f9c96582eb51ec1596a8e5c80d5c7f67d684c29
fajarnashirul <70890262+fajarnashirul@users.noreply.github.com>
2022-04-15 06:40:04 +0000

    refactor model for menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_061941) do
+ActiveRecord::Schema.define(version: 2022_04_15_063643) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
+    t.text "description"
     t.float "price"
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
