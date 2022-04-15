b0c60ff8c17e37bdd4f331f9212fdfa6115a9c46
fajarnashirul <70890262+fajarnashirul@users.noreply.github.com>
2022-04-15 07:15:10 +0000

    create test validation for Menu with specification: name: can't be blank and duplicate description: can't greater than 150 character price: can't less than 0.01

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,7 @@
 class Menu < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  
+  validates :price, comparison: { greater_than_or_equal_to: 0.01 }
+
+  validates :description, length: { maximum: 150, too_long: "description must less than 150 character"}
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should valid factory bot' do
+    expect(FactoryBot.build(:menu)).to be_valid
+  end
+  
+  it 'should invalid with duplicate name ' do
+    menu1 = FactoryBot.create(:menu, name:"Nasi Uduk")
+    menu2 = FactoryBot.build(:menu, name:"Nasi Uduk")
+
+    menu2.valid?
+    expect(menu2.errors[:name]).to include("has already been taken")
+  end
+  
+  it 'should invalid with blank name' do
+    menu = FactoryBot.build(:menu, name: nil)
+    
+    menu.valid?
+    
+    expect(menu.errors[:name]).to include("can't be blank")
+  end
+
+  it 'should invalid with price less than 0.01' do
+    menu = FactoryBot.build(:menu, price: 0.001)
+    
+    menu.valid?
+    
+    expect(menu.errors[:price]).to include("must be greater than or equal to 0.01")
+  end
+
+  it 'should invalid with description more than 150 character' do
+    menu = FactoryBot.build(:menu, description: "Nasi kuning, sesuai dengan namanya merupakan nasi yang berwarna kuning dengan menggunakan pewarna makanan alami yakni kunyit. Selain warnanya yang berbeda dengan nasi biasanya adalah rasa yang gurih serta aroma yang harum.")
+    
+    menu.valid?
+    
+    expect(menu.errors[:description]).to include("description must less than 150 character")
+  end
+  
 end
