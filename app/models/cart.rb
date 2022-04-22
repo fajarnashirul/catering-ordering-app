f96514fead2a18912f828f1805f083bc0bb62963
fajarnashirul <70890262+fajarnashirul@users.noreply.github.com>
2022-04-22 16:42:05 +0000

    cart can sum total price of each menu and order details can total the price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,13 @@
 class Cart < ApplicationRecord
+  has_many :order_details, dependent: :destroy
+  has_many :menus, through: :order_details
+
+  # LOGIC
+  def sub_total
+    sum = 0
+    self.order_details.each do |order_detail|
+      sum+= order_detail.total_price
+    end
+    return sum
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,10 @@
 class OrderDetail < ApplicationRecord
   belongs_to :cutomer_order
   belongs_to :menu
+  belongs_to :cart
+  
+  def total_price
+    self.quantity * self.menu.price
+  end
+
 end
